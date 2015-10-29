8d80f2ade7456d9405413fed09e99d26aa1484da
brdo5692 <brdo5692@colorado.edu>
2015-10-29 15:50:39 -0600

    Simple main.cpp stuff.

--- Final_Project/teensy_lib/src/main.cpp
+++ Final_Project/teensy_lib/src/main.cpp
@@ -4,10 +4,22 @@ extern "C"
 
 int main(void) {
 	pinMode(13, OUTPUT);
+	uint32_t i = 0;
 	while (1) {
 		digitalWriteFast(13, HIGH);
 		delay(100);
 		digitalWriteFast(13, LOW);
 		delay(100);
+		digitalWriteFast(13, HIGH);
+		delay(100);
+		digitalWriteFast(13, LOW);
+		delay(100);
+		digitalWriteFast(13, HIGH);
+		delay(1000);
+		digitalWriteFast(13, LOW);
+		delay(1000);
+		Serial.print("Good Test ");
+		Serial.println(i);
+		i++;
 	}
 }
