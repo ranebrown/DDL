5c70291eff80e352edc1f71eb14032248b72464f
ranebrown <rane.brown@gmail.com>
2015-10-29 14:02:12 -0600

    Teensy template

---
+++ Final Project/teensy-template/src/main.cpp
@@ -0,0 +1,13 @@
+#include "WProgram.h"
+
+extern "C" 
+
+int main(void) {
+	pinMode(13, OUTPUT);
+	while (1) {
+		digitalWriteFast(13, HIGH);
+		delay(100);
+		digitalWriteFast(13, LOW);
+		delay(100);
+	}
+}
