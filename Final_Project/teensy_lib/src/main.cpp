dbf8ef21189c96bb03db7e6f1992e1fc63cfb51f
brdo5692 <brdo5692@colorado.edu>
2015-11-04 18:12:22 -0700

    Fixed a little bug in main.cpp of the teensy source

--- Final_Project/teensy_lib/src/main.cpp
+++ Final_Project/teensy_lib/src/main.cpp
@@ -1,6 +1,6 @@
 #include "WProgram.h"
 
-extern "C" 
+extern "C"
 
 #define Bluetooth Serial1 // using serial port 1 for bluetooth communication
 
@@ -10,34 +10,34 @@ int main(void) {
     int adcVal = 0;
     pinMode(A0,INPUT); // set analog pin 0 as input
     pinMode(13, OUTPUT);
-	uint32_t i = 0;
+    uint32_t i = 0;
 
-	while (1) {
-		if(Bluetooth.available() > 0) {
-			c = Bluetooth.read();
-			Bluetooth.print(c);
-		}
-		if(c == 'r') {
-			adcVal = analogRead(0); // analog reading on A0
-			Bluetooth.println(adcVal);
-			c = 'a';
-	}
-
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(100);
-		digitalWriteFast(13, LOW);
-		delay(100);
-		digitalWriteFast(13, HIGH);
-		delay(100);
-		digitalWriteFast(13, LOW);
-		delay(100);
-		digitalWriteFast(13, HIGH);
-		delay(1000);
-		digitalWriteFast(13, LOW);
-		delay(1000);
-		Serial.print("Good Test ");
-		Serial.println(i);
-		i++;
-	}
+    while (1) {
+        if(Bluetooth.available() > 0) {
+            c = Bluetooth.read();
+            Bluetooth.print(c);
+        }
+        if(c == 'r') {
+            adcVal = analogRead(0); // analog reading on A0
+            Bluetooth.println(adcVal);
+            c = 'a';
+        }
+    }
+    while (1) {
+        digitalWriteFast(13, HIGH);
+        delay(100);
+        digitalWriteFast(13, LOW);
+        delay(100);
+        digitalWriteFast(13, HIGH);
+        delay(100);
+        digitalWriteFast(13, LOW);
+        delay(100);
+        digitalWriteFast(13, HIGH);
+        delay(1000);
+        digitalWriteFast(13, LOW);
+        delay(1000);
+        Serial.print("Good Test ");
+        Serial.println(i);
+        i++;
+    }
 }
